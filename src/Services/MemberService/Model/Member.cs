e519ec79e05fdcf798b24664150e73cc4be100f9
Quang Pho <quang.pho@yahoo.com>
2025-03-22 16:57:01 +0700

    Update member model

--- src/Services/MemberService/Model/Member.cs
+++ src/Services/MemberService/Model/Member.cs
@@ -4,8 +4,8 @@ namespace Model;
 
 public class Member
 {
-    [JsonProperty("id")]
     public string Id { get; set; }
     public string Name { get; set; }
     public string GroupdId { get; set; }
+    public string PartionKey { get; set; }
 }
